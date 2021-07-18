d3ee622a93c091b137f34f424dd210b7b461c07e
jhansi <62535629+jhansi16@users.noreply.github.com>
2021-07-18 18:49:52 +0530

    Add files via upload

---
+++ SMART AI CHATBOT -MALVIKA SINGH |20BCE0857/chatbot.py
@@ -0,0 +1,114 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+from keras.models import load_model
+model = load_model('chatbot_model.h5')
+import json
+import random
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+
+def cleanupsentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return  words array: 0 or 1 for word that exists in the sentence
+
+def wor(sentence, words, show_details=True):
+    sentence_words = cleanupsentence(sentence)
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predictclass(sentence, model):
+    p = wor(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getRes(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predictclass(msg, model)
+    res = getRes(ints, intents)
+    return res
+
+
+#Creating GUI with tkinter
+import tkinter
+from tkinter import *
+
+
+def send():
+    msg = EntryBox.get("1.0",'end-1c').strip()
+    EntryBox.delete("0.0",END)
+
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#ffffff", font=("Verdana", 12 ))
+
+        res = chatbot_response(msg)
+        ChatLog.insert(END, "Bot: " + res + '\n\n')
+
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+#creating main window object
+base = Tk()
+base.title("chatbox")
+base.geometry("400x470")
+base.resizable(width=FALSE, height=FALSE)
+
+#Create Chat window
+ChatLog = Text(base, bd=0, bg="black", height="8", width="50", font="Arial",)
+ChatLog.config(state=NORMAL)
+ChatLog.insert(END, "Welcome, Dear user"+'\n\n')
+ChatLog.config(foreground="#ffffff", font=("Calibre", 12 ))
+
+ChatLog.config(state=DISABLED)
+
+#Bind scrollbar to Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+#Create Button to send message
+SendButton = Button(base, font=("Calibre",12,'bold'), text="Send", width="10", height=2,
+                    bd=0, bg="#340a8a", activebackground="#40f7f4",fg='#ffffff',
+                    command= send )
+
+#Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white",width="15", height="2", font="Calibre")
+#EntryBox.bind("<Return>", send)
+
+
+#Place all components on the screen
+scrollbar.place(x=376,y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=370)
+EntryBox.place(x=6, y=401, height=60, width=265)
+SendButton.place(x=270, y=401, height=60)
+
+base.mainloop()
